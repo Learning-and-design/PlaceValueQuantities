b8c4f63ebb43ff39fc2d5503ac4353c7f8c54875
kartik4f <kartik.singh4f@gmail.com>
2021-05-21 11:44:54 +0530

    initial commit

--- scripts/project/main.js
+++ scripts/project/main.js
@@ -1,25 +1,122 @@
 
-// Import any other script files here, e.g.:
-// import * as myModule from "./mymodule.js";
+
+
+var messageData;
+// localStorage.clear();
+// Uncomment if testing without app
+var message = {"data":{}};
+
+message.data = {"learningTrackid":1,"gameId":1,"gameVersion":"string","predGameId":0,"gamePath":"https://kreedo-game-upload-poc.s3.us-east-2.amazonaws.com/701_LearningTeens.zip","isActive":true,"isblocked":false,"isGameDownloadComplete":true,"gameName":"Place Value Quantities","attemptId":0,"totalRewards":0,"completedCount":0,"startDateTime":"","endDateTime":"","completed":false,"isMusic":true,"levelDetails":{"currentLevel":{"level":0,"presentationCompleted":0},"level0":{"presentation":{"completed":0,"playCount":0,"completedCount":0,"timeSpent":0}},"level1":{"presentation":{"completed":0,"playCount":0,"completedCount":0,"timeSpent":0},"completed":0,"playCount":0,"completedCount":0,"timeSpent":0,"correctAttempts":0,"incorrectAttempts":0},"level2":{"presentation":{"completed":0,"playCount":0,"completedCount":0,"timeSpent":0},"completed":0,"playCount":0,"completedCount":0,"timeSpent":0,"correctAttempts":0,"incorrectAttempts":0},"level3":{"presentation":{"completed":0,"playCount":0,"completedCount":0,"timeSpent":0},"completed":0,"playCount":0,"completedCount":0,"timeSpent":0,"correctAttempts":0,"incorrectAttempts":0},"level4":{"presentation":{"completed":0,"playCount":0,"completedCount":0,"timeSpent":0},"completed":0,"playCount":0,"completedCount":0,"timeSpent":0,"correctAttempts":0,"incorrectAttempts":0}}};
+
+var levelDetails = {"currentLevel":{"level":0,"presentationCompleted":0},"level0":{"presentation":{"completed":0,"playCount":0,"completedCount":0,"timeSpent":0}},"level1":{"presentation":{"completed":0,"playCount":0,"completedCount":0,"timeSpent":0},"completed":0,"playCount":0,"completedCount":0,"timeSpent":0,"correctAttempts":0,"incorrectAttempts":0},"level2":{"presentation":{"completed":0,"playCount":0,"completedCount":0,"timeSpent":0},"completed":0,"playCount":0,"completedCount":0,"timeSpent":0,"correctAttempts":0,"incorrectAttempts":0},"level3":{"presentation":{"completed":0,"playCount":0,"completedCount":0,"timeSpent":0},"completed":0,"playCount":0,"completedCount":0,"timeSpent":0,"correctAttempts":0,"incorrectAttempts":0},"level4":{"presentation":{"completed":0,"playCount":0,"completedCount":0,"timeSpent":0},"completed":0,"playCount":0,"completedCount":0,"timeSpent":0,"correctAttempts":0,"incorrectAttempts":0}}
+
+//uncomment this block to mimic the data comming from app
+handleEvent(message);
+
+//app will trigger this event on load finish.
+// comment this listener if testing without app
+// document.addEventListener("message", handleEvent);
+
+function handleEvent(message) {	
+	messageData =  message.data;	
+
+	
+}
 
 runOnStartup(async runtime =>
 {
-	// Code to run on the loading screen.
-	// Note layouts, objects etc. are not yet available.
-	const url_string =window.location.href;
-	const url = new URL(url_string);
-	const jsonStr = url.searchParams.get("json");
-	console.log("json string::",jsonStr);
+	if(localStorage.getItem("pvq-data")!=null){
 	
-	if(jsonStr){
-		const gameJson = JSON.parse(jsonStr);
-		runtime.globalVars.GameID = gameJson.id;
-		runtime.globalVars.ChildID = gameJson.childId;
-		runtime.globalVars.GameName = gameJson.name;
-		runtime.globalVars.AttemptID = gameJson.attemptId;
+	var jsonTxt = localStorage.getItem("pvq-data");
+		let data = JSON.parse(localStorage.getItem("pvq-data"))	
+		messageData=data;
+		console.log("Loaded from LocalStorage", data);
 	}
-	runtime.addEventListener("beforeprojectstart", () => OnBeforeProjectStart(runtime));
-	
+	else{
+	console.log("loaded from app ", messageData);
+	}
+	if(messageData){
+		console.log("loaded data: ", JSON.stringify(messageData));
+		messageData.levelDetails.level0 = levelDetails.level0;
+		messageData.levelDetails.level1 = levelDetails.level1;
+		messageData.levelDetails.level2 = levelDetails.level2;
+		messageData.levelDetails.level3 = levelDetails.level3;
+		messageData.levelDetails.level4 = levelDetails.level4;
+		console.log("Default data: ", JSON.stringify(messageData));
+		runtime.globalVars.GameID = messageData.gameId;
+// 		runtime.globalVars.ChildID = messageData.childId;
+		runtime.globalVars.GameName = messageData.gameName;		
+		runtime.globalVars.AttemptID = messageData.attemptId;
+		
+		runtime.globalVars.RewardPoints = messageData.totalRewards;
+		runtime.globalVars.isMusic = messageData.isMusic;		
+        runtime.globalVars.isGameCompleted = messageData.completed;
+		runtime.globalVars.GameCompletedCounter = messageData.completedCount;
+		//current level
+		runtime.globalVars.GameLevel = messageData.levelDetails.currentLevel.level;
+// 		runtime.globalVars.isGamePresentatinCompleted = messageData.levelDetails.currentLevel.presentationCompleted;
+		
+        if(runtime.globalVars.isGameCompleted)
+        {
+            runtime.globalVars.L4TutorialDone = 1
+			runtime.globalVars.L3TutorialDone = 1;
+			runtime.globalVars.L2TutorialDone = 1;
+			runtime.globalVars.L1TutorialDone = 1;						
+			runtime.globalVars.L0TutorialDone = 1;
+
+			runtime.globalVars.L1_Completed = 1;
+			runtime.globalVars.L2_Completed = 1;
+			runtime.globalVars.L3_Completed = 1;
+            runtime.globalVars.L4_Completed = 1;
+
+            ;
+        }
+        else{
+            if(runtime.globalVars.GameLevel == 0){
+                runtime.globalVars.L0TutorialDone =messageData.levelDetails.currentLevel.presentationCompleted
+             //remaining are false by default
+            }
+            else if(runtime.globalVars.GameLevel == 1){
+                runtime.globalVars.L0TutorialDone = 1;
+                runtime.globalVars.L1TutorialDone = messageData.levelDetails.currentLevel.presentationCompleted	
+                
+                }
+            else if(runtime.globalVars.GameLevel == 2){
+                runtime.globalVars.L2TutorialDone = messageData.levelDetails.currentLevel.presentationCompleted;
+                runtime.globalVars.L1TutorialDone = 1;			
+                runtime.globalVars.L0TutorialDone = 1;
+                
+                runtime.globalVars.L1_Completed = 1;
+                }
+            else if(runtime.globalVars.GameLevel == 3){
+                runtime.globalVars.L3TutorialDone = messageData.levelDetails.currentLevel.presentationCompleted
+                runtime.globalVars.L2TutorialDone = 1;
+                runtime.globalVars.L1TutorialDone = 1;
+                runtime.globalVars.L0TutorialDone = 1;
+                
+                
+                runtime.globalVars.L1_Completed = 1;
+                runtime.globalVars.L2_Completed = 1;
+                }
+            else if(runtime.globalVars.GameLevel == 4){
+                runtime.globalVars.L4TutorialDone = messageData.levelDetails.currentLevel.presentationCompleted
+                runtime.globalVars.L3TutorialDone = 1;
+                runtime.globalVars.L2TutorialDone = 1;
+                runtime.globalVars.L1TutorialDone = 1;						
+                runtime.globalVars.L0TutorialDone = 1;
+
+                runtime.globalVars.L1_Completed = 1;
+                runtime.globalVars.L2_Completed = 1;
+                runtime.globalVars.L3_Completed = 1;			
+                }
+            else {
+        		console.log("current level "+runtime.globalVars.GameLevel);
+            }
+        }
+			
+				   
+	}
+	runtime.addEventListener("beforeprojectstart", () => OnBeforeProjectStart(runtime));	
 });
 
 async function OnBeforeProjectStart(runtime)
@@ -27,7 +124,9 @@ async function OnBeforeProjectStart(runtime)
 	// Code to run just before 'On start of layout' on
 	// the first layout. Loading has finished and initial
 	// instances are created and available to use here.
-	console.log("OnBeforeProjectStart", window.location);
+// 	console.log("OnBeforeProjectStart", window.location);
+	runtime.getInstanceByUid(486).setJsonDataCopy(messageData);
+	console.log("JSON_DATA", runtime.getInstanceByUid(486).getJsonDataCopy())
 	runtime.addEventListener("tick", () => Tick(runtime));
 }
 
--- scripts/project/scriptsInEvents.js
+++ scripts/project/scriptsInEvents.js
@@ -8,133 +8,70 @@ export class AppInterface  {
          Android.showToast(msg);
 	}
 	static sendToApp(data){		
-		let jsonTxt = JSON.stringify(data);
-		console.log("send to app",jsonTxt);
-		if(typeof(Android)!="undefined")
-			Android.showData(jsonTxt);
+		let json_str = JSON.stringify(data);
+		console.log("Send data to Webview", json_str);
+		if (window && window.ReactNativeWebView) {
+			window.ReactNativeWebView.postMessage(json_str);
+		}
+		
+
 	}
 }
+
+
+
 
 {
 	const scriptsInEvents = {
 
-		async Egame_Event1_Act8(runtime, localVars)
+		async Egame_Event1_Act9(runtime, localVars)
 		{
-			if(localStorage.getItem(runtime.globalVars.GameID+"_"+runtime.globalVars.ChildID)===null){
+			// localstorage.clear();
+			// console.log("log", runtime.getInstanceByUid(486).getJsonDataCopy()); 
+			
 			runtime.goToLayout("Tutorial");
-			}
 			
+		},
+
+		async Egame_Event11_Act1(runtime, localVars)
+		{
+			localStorage.setItem("pvq-data", JSON.stringify(runtime.getInstanceByUid(486).getJsonDataCopy()));
+			AppInterface.sendToApp(runtime.getInstanceByUid(486).getJsonDataCopy());
+		},
+
+		async Egame_Event33_Act1(runtime, localVars)
+		{
+			
+		},
+
+		async Etutorial_Event4_Act4(runtime, localVars)
+		{
+			if(eval("runtime.globalVars.L"+runtime.globalVars.GameLevel+"TutorialDone")){
+			runtime.goToLayout("Level"+runtime.globalVars.GameLevel)
+			}
 			else{
-			
-				var jsonTxt = localStorage.getItem(runtime.globalVars.GameID+"_"+runtime.globalVars.ChildID);
-			
-				var jObj = JSON.parse(jsonTxt);
-				runtime.globalVars.RewardPoints = jObj.RewardPoints;
-			    //Presenation level
-				runtime.globalVars.PresentationDone = jObj.Level0.Completed;	
-			    runtime.globalVars.L0_START_TIME = jObj.Level0.StartTime;
-				runtime.globalVars.L0_END_TIME = jObj.Level0.EndTime;
-				runtime.globalVars.L0_PLAY_COUNT = jObj.Level0.PlayCount;
-				runtime.globalVars.isMusic = jObj.isMusic;
-			
-				//set level 1 global vars
-				runtime.globalVars.L1TutorialDone = jObj.Level1.PresentationDone;
-				runtime.globalVars.L1_TotalTrial = jObj.Level1.PlayCount;
-				runtime.globalVars.L1_trial = jObj.Level1.CurrentPlay;
-				runtime.globalVars.L1_Completed = jObj.Level1.Completed;
-				runtime.globalVars.L1_correctAttempts = jObj.Level1.CorrectAttempts;
-				runtime.globalVars.L1_IncorrectAttempts = jObj.Level1.IncorrectAttempts;
-			
-				//set level 2 global vars
-				runtime.globalVars.L2TutorialDone = jObj.Level2.PresentationDone;
-				runtime.globalVars.L2_TotalTrial = jObj.Level2.PlayCount;
-				runtime.globalVars.L2_trial = jObj.Level2.CurrentPlay;
-				runtime.globalVars.L2_Completed = jObj.Level2.Completed;
-				runtime.globalVars.L2_correctAttempts = jObj.Level2.CorrectAttempts;
-				runtime.globalVars.L2_IncorrectAttempts = jObj.Level2.IncorrectAttempts;
-			
-					runtime.globalVars.L3TutorialDone = jObj.Level3.PresentationDone;
-				runtime.globalVars.L3_TotalTrial = jObj.Level3.PlayCount;
-				runtime.globalVars.L3_trial = jObj.Level3.CurrentPlay;
-				runtime.globalVars.L3_Completed = jObj.Level3.Completed;
-				runtime.globalVars.L3_correctAttempts = jObj.Level3.CorrectAttempts;
-				runtime.globalVars.L3_IncorrectAttempts = jObj.Level3.IncorrectAttempts;
-			
-					runtime.globalVars.L4TutorialDone = jObj.Level4.PresentationDone;
-				runtime.globalVars.L4_TotalTrial = jObj.Level4.PlayCount;
-				runtime.globalVars.L4_trial = jObj.Level4.CurrentPlay;
-				runtime.globalVars.L4_Completed = jObj.Level4.Completed;
-				runtime.globalVars.L4_correctAttempts = jObj.Level4.CorrectAttempts;
-				runtime.globalVars.L4_IncorrectAttempts = jObj.Level4.IncorrectAttempts;
-				
-				if(!jObj.Level0.Completed){
-			
-					runtime.goToLayout("Tutorial");
-					return;
-				}
-				for(let i=1; i<=4 ;i++){
-					if(!eval("runtime.globalVars.L"+i+"TutorialDone")){
-						runtime.goToLayout("Level"+i+"_Tutorial");
-						return;
-					}
-					else if(!eval("runtime.globalVars.L"+i+"_Completed")){
-						runtime.goToLayout("Level"+i);
-					}
-				}
-			
+			runtime.goToLayout("Level"+runtime.globalVars.GameLevel+"_Tutorial");
 			}
 			
 		},
 
-		async Egame_Event4_Act1(runtime, localVars)
+		async Etutorial_Event5_Act1(runtime, localVars)
 		{
-			var data = {
-			GameName:runtime.globalVars.GameName,
-			GameID:runtime.globalVars.GameID,
-			ChildID: runtime.globalVars.ChildID,
-			AttemptID:runtime.globalVars.AttemptID,
-			RewardPoints: runtime.globalVars.RewardPoints,
-			isMusic: runtime.globalVars.isMusic,
-				Level0:{										Completed:runtime.globalVars.PresentationDone,
-				StartTime: runtime.globalVars.L0_START_TIME,
-				EndTime: runtime.globalVars.L0_END_TIME,
-				PlayCount: runtime.globalVars.L0_PLAY_COUNT
-				
-			},
-				Level1: {PresentationDone: runtime.globalVars.L1TutorialDone, Completed:runtime.globalVars.L1_Completed, PlayCount: runtime.globalVars.L1_TotalTrial, CurrentPlay: runtime.globalVars.L1_trial, CorrectAttempts: runtime.globalVars.L1_correctAttempts, IncorrectAttempts: runtime.globalVars.L1_IncorrectAttempts},
-			
-			Level2:{PresentationDone: runtime.globalVars.L2TutorialDone, Completed:runtime.globalVars.L2_Completed, PlayCount: runtime.globalVars.L2_TotalTrial,
-			CurrentPlay: runtime.globalVars.L2_trial, CorrectAttempts: runtime.globalVars.L2_correctAttempts, IncorrectAttempts: runtime.globalVars.L2_IncorrectAttempts},
-			
-			Level3:{PresentationDone: runtime.globalVars.L3TutorialDone, Completed:runtime.globalVars.L3_Completed, PlayCount: runtime.globalVars.L3_TotalTrial, CurrentPlay: runtime.globalVars.L3_trial, CorrectAttempts: runtime.globalVars.L3_correctAttempts, IncorrectAttempts: runtime.globalVars.L3_IncorrectAttempts},
-			
-			Level4:{PresentationDone: runtime.globalVars.L4TutorialDone, Completed:runtime.globalVars.L4_Completed, PlayCount: runtime.globalVars.L4_TotalTrial,CurrentPlay: runtime.globalVars.L4_trial, CorrectAttempts: runtime.globalVars.L4_correctAttempts, IncorrectAttempts: runtime.globalVars.L4_IncorrectAttempts}
-			
+			if(eval("runtime.globalVars.L"+runtime.globalVars.GameLevel+"TutorialDone")){
+			runtime.goToLayout("Level"+runtime.globalVars.GameLevel)
+			}
+			else{
+			runtime.goToLayout("Level"+runtime.globalVars.GameLevel+"_Tutorial");
 			}
 			
-			var json = JSON.stringify(data);
-			
-			localStorage.setItem(runtime.globalVars.GameID+"_"+runtime.globalVars.ChildID, json);
-			AppInterface.sendToApp(data);
 		},
 
-		async Egame_Event20_Act1(runtime, localVars)
+		async Elevel3_Event98_Act4(runtime, localVars)
 		{
 			
-		},
-
-		async Etutorial_Event4_Act2(runtime, localVars)
-		{
-				for(let i=1; i<=4 ;i++){
-					if(!eval("runtime.globalVars.L"+i+"TutorialDone")){
-						runtime.goToLayout("Level"+i+"_Tutorial");
-						return;
-					}
-					else if(!eval("runtime.globalVars.L"+i+"_Completed")){
-						runtime.goToLayout("Level"+i);
-					}
-				}
-			
+			let isMusic = localStorage.getItem(runtime.globalVars.GameName+"_Music");
+			console.log(isMusic);
+			runtime.globalVars.isMusic = isMusic;
 		}
 
 	};
