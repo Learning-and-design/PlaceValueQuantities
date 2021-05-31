d56747bd65dfe8da24599d3d7d0e01336661b0f2
kartik4f <kartik.singh4f@gmail.com>
2021-05-31 18:54:00 +0530

    home button exit and start & end  date-time added and browser close added

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,91 @@
+"use strict";
+
+export class AppInterface  {
+
+	static showToast(msg){
+		let jsonToast = {};
+			jsonToast.toast = msg;
+        console.log('showToast', jsonToast);
+		   if (window && window.ReactNativeWebView) {
+		   
+			window.ReactNativeWebView.postMessage(jsonToast);
+		}
+		
+	}
+	static sendToApp(data){		
+		let json_str = JSON.stringify(data);
+		console.log("Send data to Webview", json_str);
+		if (window && window.ReactNativeWebView) {
+			window.ReactNativeWebView.postMessage(json_str);
+		}
+		
+
+	}
+}
+
+
+
+
+{
+	const scriptsInEvents = {
+
+		async Egame_Event1_Act9(runtime, localVars)
+		{
+			// localstorage.clear();
+			// console.log("log", runtime.getInstanceByUid(486).getJsonDataCopy()); 
+			
+			runtime.goToLayout("Tutorial");
+			
+		},
+
+		async Egame_Event11_Act1(runtime, localVars)
+		{
+			localStorage.setItem("pvq-data", JSON.stringify(runtime.getInstanceByUid(486).getJsonDataCopy()));
+			AppInterface.sendToApp(runtime.getInstanceByUid(486).getJsonDataCopy());
+		},
+
+		async Egame_Event12_Act5(runtime, localVars)
+		{
+			localStorage.setItem("pvq-data", JSON.stringify(runtime.getInstanceByUid(486).getJsonDataCopy()));
+			AppInterface.sendToApp(runtime.getInstanceByUid(488).getJsonDataCopy())
+		},
+
+		async Egame_Event34_Act1(runtime, localVars)
+		{
+			
+		},
+
+		async Etutorial_Event4_Act4(runtime, localVars)
+		{
+			if(eval("runtime.globalVars.L"+runtime.globalVars.GameLevel+"TutorialDone")){
+			runtime.goToLayout("Level"+runtime.globalVars.GameLevel)
+			}
+			else{
+			runtime.goToLayout("Level"+runtime.globalVars.GameLevel+"_Tutorial");
+			}
+			
+		},
+
+		async Etutorial_Event5_Act1(runtime, localVars)
+		{
+			if(eval("runtime.globalVars.L"+runtime.globalVars.GameLevel+"TutorialDone")){
+			runtime.goToLayout("Level"+runtime.globalVars.GameLevel)
+			}
+			else{
+			runtime.goToLayout("Level"+runtime.globalVars.GameLevel+"_Tutorial");
+			}
+			
+		},
+
+		async Elevel3_Event98_Act4(runtime, localVars)
+		{
+			
+			let isMusic = localStorage.getItem(runtime.globalVars.GameName+"_Music");
+			console.log(isMusic);
+			runtime.globalVars.isMusic = isMusic;
+		}
+
+	};
+	
+	self.C3.ScriptsInEvents = scriptsInEvents;
+}
